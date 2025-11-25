e8be99ce8168f41190bb7d7dcdcafa4408a13b18
Whendell <whndllsnts@gmail.com>
2025-11-26 03:02:14 +0800

    A fully functional, two-team Basketball Scoreboard built using HTML, CSS, and Vanilla JavaScript. The project utilizes id attributes and direct onclick() handlers to manage real-time scoring

---
+++ script.js
@@ -0,0 +1,46 @@
+let homeScorePoint = document.getElementById("home-score-point");
+let guestScorePoint = document.getElementById("guest-score-point");
+let homeScore = 0
+let guestScore = 0
+
+// home function
+function homeScoreOne() {
+  homeScore += 1
+  homeScorePoint.textContent = homeScore
+}
+
+function homeScoreTwo() {
+  homeScore += 2
+  homeScorePoint.textContent = homeScore
+}
+
+function homeScoreThree() {
+  homeScore += 3
+  homeScorePoint.textContent = homeScore
+}
+
+function homeDecrement() {
+  homeScore -= 1
+  homeScorePoint.textContent = homeScore
+}
+
+// guest function
+function guestScoreOne() {
+  guestScore += 1
+  guestScorePoint.textContent = guestScore
+}
+
+function guestScoreTwo() {
+  guestScore += 2
+  guestScorePoint.textContent = guestScore
+}
+
+function guestScoreThree() {
+  guestScore += 3
+  guestScorePoint.textContent = guestScore
+}
+
+function guestDecrement() {
+  guestScore -= 1
+  guestScorePoint.textContent = guestScore
+}
